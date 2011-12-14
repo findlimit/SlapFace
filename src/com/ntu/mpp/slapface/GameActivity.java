2bf6b18c173452a1d000e37c085457819f6fb8f5
CHA <r00942119@ntu.edu.tw>
2011-12-14 17:39:59 +0800

    Modified GameActivity By CHA; Version 0.5

--- src/com/ntu/mpp/slapface/GameActivity.java
+++ src/com/ntu/mpp/slapface/GameActivity.java
@@ -16,11 +16,15 @@ public class GameActivity extends Activity {
 			switch(msg.what){
 			case 0:
 				// TODO: Call attack method 
+				attack(Double.parseDouble(msg.obj.toString()));
 				break;
 			case 1:
 				// TODO: Call defend method
 				break;
+			case 2:
+				// TODO: Run when attacker waiting for response from defender
 			default:
+				getRespond(Integer.parseInt(msg.obj.toString()));
 				break;
 			}
 		}
@@ -29,7 +33,8 @@ public class GameActivity extends Activity {
 	private Thread readThread;
 	private boolean host;
 	
-	boolean isHost;
+	boolean isHost;//True: Host, False: client.
+	boolean adState;//True: Attacker, False: Defender.
 	private int myHp;
 	private int enemyHp;
 	
@@ -44,14 +49,43 @@ public class GameActivity extends Activity {
 		if (host){
 			readThread = new Thread(new HostReadThread(mHandler));
 			isHost=true;
+			adState=true;
 		}else{
 			readThread = new Thread(new ClientReadThread(mHandler));
 			isHost=false;
+			adState=false;
 		}
 		readThread.start();
 		myHp=100;
 		enemyHp=100;
 	}
 	
+	private void reflash(){//used to reflash view
+	}
 	
+	
+	private void attack(double pow){
+		//mHandler.sendMessage();
+		sendMsg(1, pow);
+	}
+
+	private void getRespond(int eHP){
+		enemyHp=eHP;
+		if(enemyHp<=0){
+			isGameOver(true);
+		}else{
+			reflash();
+			adState=true;
+			waitThreeSecond();
+		}
+	}
+	
+	private void waitThreeSecond(){//used to wait three second
+	}
+	private void isGameOver(boolean isYouWin){//isYouWin==true: attacker win
+	}
+	//////////////////////////////////////////////////////////
+	private void sendMsg(int type, Object pow) {//TODO: Peter's work
+		// TODO Auto-generated method stub
+	}
 }
