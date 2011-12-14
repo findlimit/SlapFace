0767e7cb9649c477ec9ce7d4d5d9bb6e9837959d
CHA <r00942119@ntu.edu.tw>
2011-12-14 17:04:17 +0800

    Modified GameActivity version 0.1

--- src/com/ntu/mpp/slapface/GameActivity.java
+++ src/com/ntu/mpp/slapface/GameActivity.java
@@ -12,11 +12,26 @@ public class GameActivity extends Activity {
 		public void handleMessage(Message msg) {
 			// TODO Auto-generated method stub
 			super.handleMessage(msg);
+			
+			switch(msg.what){
+			case 0:
+				// TODO: Call attack method 
+				break;
+			case 1:
+				// TODO: Call defend method
+				break;
+			default:
+				break;
+			}
 		}
 	};
 	
 	private Thread readThread;
 	private boolean host;
+	
+	boolean isHost;
+	private int myHp;
+	private int enemyHp;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +41,17 @@ public class GameActivity extends Activity {
 	}
 	
 	private void init(boolean host) {
-		if (host)
+		if (host){
 			readThread = new Thread(new HostReadThread(mHandler));
-		else
+			isHost=true;
+		}else{
 			readThread = new Thread(new ClientReadThread(mHandler));
+			isHost=false;
+		}
 		readThread.start();
-		
+		myHp=100;
+		enemyHp=100;
 	}
+	
+	
 }
